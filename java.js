3846730cef37e66149afc4d60bd27c086404ae19
memmedlileman <memmedileman02@gmail.com>
2024-03-26 13:59:54 +0400

    task12

---
+++ java.js
@@ -0,0 +1,29 @@
+// let name = prompt("Adinizi daxil edin: ");
+// console.log(name.length);
+
+// let soz = prompt("1ci sozu daxil edin: ");
+// let  herf = prompt("herfi daxil edin daxil edin: ");
+// let netice = soz.includes(herf);
+// console.log(netice);
+
+
+// let soz = prompt("1ci sozu daxil edin: ");
+// let  herf = prompt("herfi daxil edin daxil edin: ");
+// let netice = soz.startsWith(herf);
+// console.log(netice);
+
+
+// let soz = prompt("1ci sozu daxil edin: ");
+// let  herf = prompt("herfi daxil edin daxil edin: ");
+// let netice = soz.endsWith(herf);
+// console.log(netice);
+
+// let soz = prompt("1ci sozu daxil edin: ");
+// let netice = /^(?=[A-Z])[A-Z\s]+ $/.test(soz);
+// console.log(netice);
+
+// let soz = prompt("1ci sozu daxil edin: ");
+// let  herf = prompt("herfi daxil edin daxil edin: ");
+// let netice = soz.indexOf(herf);
+// console.log(netice);
+
